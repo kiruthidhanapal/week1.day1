ac6156cbfa64e37f58d171ad4d9aa457bac1d925
kiruthidhanapal <104193782+kiruthidhanapal@users.noreply.github.com>
2022-04-23 08:55:03 +0530

    Add files via upload

---
+++ Assingnment.java
@@ -0,0 +1,14 @@
+package assignment;
+	public class Assingnment{
+	public static void main(String args[])
+	{
+	int i =40;
+	if(i>0)
+	{System.out.println("the number is positve");
+	}
+	}
+	else{
+		System.out.println("the number is negative" )
+	}
+	}
+	}
